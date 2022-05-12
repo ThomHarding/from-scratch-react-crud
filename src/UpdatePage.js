eebf00f5d799714dbc9806207af7df4954d4987a
THOMAS HARDING <hardingt604@gmail.com>
2022-05-12 16:24:51 -0600

    added handleupdate function to updatepage.js

--- src/UpdatePage.js
+++ src/UpdatePage.js
@@ -6,6 +6,7 @@ import { updatePony } from './services/fetch-utils';
 export default function DetailPage() {
   const [pony, setPony] = useState({});
   const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [kind, setKind] = useState('');
   const [location, setLocation] = useState('');
   const [friends, setFriends] = useState('');
@@ -17,13 +18,29 @@ export default function DetailPage() {
     async function loadData() {
       const ponyData = await getPonyById(id);
       setPony(ponyData);
-      setFormName(eatery.name);
-      setFormCusine(eatery.cuisine);
-      setFormCity(eatery.city);
-      setFormPriceRating(eatery.price_rating);
+      setFirstName([pony].firstname);
+      setFirstName([pony].lastname);
+      setKind([pony].kind);
+      setLocation([pony].location);
+      setFriends([pony].friends);
+      setElement([pony].element);
     }
     loadData();
   }, [id]);
+
+  async function handleUpdate(e) {
+    e.preventDefault();
+    await updateEatery(id, {
+      firstname: firstName,
+      lastName: lastName,
+      kind: kind,
+      location: location,
+      friends: friends,
+      element: element
+    });
+
+    history.push('/ListPage');
+  }
 
   return (
     //todo: the entire thing
